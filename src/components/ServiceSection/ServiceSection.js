3c0ee52848040f4604c73ae2ec694bc603c8e4c2
Jose Pereyra <j.pereyra@sien.in>
2019-11-04 17:38:49 -0700

    gestalt

--- src/components/ServiceSection/ServiceSection.js
+++ src/components/ServiceSection/ServiceSection.js
@@ -1,30 +1,53 @@
 import React from 'react';
 import './ServiceSection.css';
+import ServicesList from './Services.json';
 
 const ServiceSection = () => {
+
+    let styleBoxContainer = {
+        backgroundColor:'transparent',
+        height:'350px',
+        marginTop: '0px',
+        width:'120px',
+        margin: '0',
+        boxSizing: 'border-box',
+        transform: 'skewX(-10deg)',
+        float:'left', 
+        position:'relative',
+        overflow:'hidden',
+        padding:'3px'
+    };
+
+    let styleBoxWrapper = {
+        backgroundColor:'red',
+        height:'320px',
+        marginTop: '0px',
+        width:'100%',
+        margin: '0',
+        boxSizing: 'border-box',
+        transform: 'skewY(0deg)',
+        float:'left', 
+        position:'absolute',
+        boxShadow:'4px 4px 3px rgba(0,0,0,.5)'
+
+    };
+
     return (
         <section className="ss-container">
-            <div style={{backgroundColor:'yellow'}}>
-                <div className="box" style={{backgroundColor:'blue'}}>
-                    <div className="wrapper">
-
-                    </div>
-                </div>
-                <div className="box" style={{backgroundColor:'cyan'}}>
-                    
-                </div>
-                <div className="box" style={{backgroundColor:'green'}}>
-                    
-                </div>
-                <div className="box" style={{backgroundColor:'red'}}>
-                    
-                </div>
-                <div className="box" style={{backgroundColor:'purple'}}>
-                    
-                </div>
+            <div>
+                {
+                    ServicesList.map( x => (
+                        <div style={{...styleBoxContainer, ...x}}>
+                            <div style={{...styleBoxWrapper, ...x}}>
+                                <div className='wrapper'>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+                
             </div>
-            
-            
         </section>
     );
 }
